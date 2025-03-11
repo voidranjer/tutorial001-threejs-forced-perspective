f5bb344d3300ceb21aa761d6e5581a8c32a48906
James <contact@jamesyap.org>
2025-03-11 12:46:51 -0400

    [1-setup] creating the scene

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 import * as THREE from "three";
+
+// three.js objects (https://threejs.org/docs/#manual/en/introduction/Creating-a-scene)
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide })
+);
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+// setup function: we call this function only once at the beginning of the program to setup the scene
+function setup() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  // add objects to the scene
+  scene.add(plane);
+  scene.add(sphere);
+
+  // move camera backwards so we can see the objects
+  camera.position.z = 5;
+}
+
+// define the animation loop function which will run ones per frame (typically 60fps)
+function animate() {
+  renderer.render(scene, camera);
+}
+
+setup(); // setup the scene
+renderer.setAnimationLoop(animate); // start the animation loop
