1aab9da8672be9cdec9be46dde8c8b34d2aec770
James <contact@jamesyap.org>
2025-03-11 14:30:12 -0400

    [2-camera] eventListeners.js

---
+++ eventListeners.js
@@ -0,0 +1,33 @@
+export function onKeyDown(event, state) {
+  switch (event.key) {
+    case "w":
+      state.keyboard.forward = true;
+      break;
+    case "s":
+      state.keyboard.backward = true;
+      break;
+    case "a":
+      state.keyboard.left = true;
+      break;
+    case "d":
+      state.keyboard.right = true;
+      break;
+  }
+}
+
+export function onKeyUp(event, state) {
+  switch (event.key) {
+    case "w":
+      state.keyboard.forward = false;
+      break;
+    case "s":
+      state.keyboard.backward = false;
+      break;
+    case "a":
+      state.keyboard.left = false;
+      break;
+    case "d":
+      state.keyboard.right = false;
+      break;
+  }
+}
